b6d5085d18e56d152bd46a5554cd12dc0b5ee005
Daniel Levine <developer148@gmail.com>
2022-12-13 16:32:42 +1300

    Danny

--- routes/index.test.js
+++ routes/index.test.js
@@ -3,16 +3,16 @@ const assert = require("assert");
 
 //Testing API endpoints
 describe("testing api endpoints '/' ", () => {
-  // it(" GET / knock knock... ", async () => {
-  //   //endpoint
-  //   return request
-  //     .get("/")
-  //     .send("whos there?")
-  //     .then((response) => {
-  //       assert.strict(response.status, 200);
-  //     });
-  // });
-  it(" POST / should return car value based on formula specified in brief", async () => {
+  it(" GET /  ", async () => {
+    //endpoint
+    return request
+      .get("/")
+      .send("Hello")
+      .then((response) => {
+        assert.strict(response.status, 200);
+      });
+  });
+  it(" POST / value returned based on formula in brief", async () => {
     //endpoint
     const model = "Civic";
     const year = 2014;
